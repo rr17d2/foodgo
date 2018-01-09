da399588e812252e24f146351ffe80a0af3d9868
rr17d2 <nikefutbol90@gmail.com>
2018-01-08 20:49:54 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,2 +1,2 @@
 
-<?php $db_connection = pg_connect("host=localhost port=5432 dbname=cmps3420 user=postgres password=Seg0nuj0048");?>
+<?php $db_connection = pg_connect("host=localhost port=5432 dbname=cmps3420 user=postgres password=askForIt");?>
